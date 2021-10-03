b05e93aef9ced1b3754d56dbefef9e7c2280b824
Redling0Botond <90475987+Redling0Botond@users.noreply.github.com>
2021-10-03 21:16:25 +0100

    Add files via upload

---
+++ TTI.py
@@ -0,0 +1,8 @@
+a=input("Kerem adja meg a magassagat m-ben:")
+b=input("Kerem adja meg a testtomeget kg-ban:")
+print(30*"*",5*"-",30*"*")
+magas=float(a)
+test=float(b)
+index=(test/magas**2.)
+print("Az on testomeg indexe:",index)
+
